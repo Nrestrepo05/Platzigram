a10ef3c25d260d89b22588de8c8de97472b5024b
Nicolás Restrepo <nrestrepo.r05@gmail.com>
2019-11-28 16:02:57 -0400

    posts models defined and registered on the admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,2 +1,13 @@
 from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class ProfileAdmin(admin.ModelAdmin):
+    
+    list_display = ('pk', 'title', 'user', 'picture',)
+    list_display_links = ('pk', 'title', 'user', 'picture')
+
+    search_fields = ('user__first_name', 'user__last_name', 'title')
+    list_filter = ('created', 'modified')
+
 
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib import admin
 from django.contrib.auth.models import User
+from django.contrib import admin
 from .models import Profile
 
 
