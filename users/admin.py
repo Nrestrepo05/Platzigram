bcac18688914455fb02e2a4ba63f949d6eb61b79
Nicolás Restrepo <nrestrepo.r05@gmail.com>
2019-11-28 14:33:28 -0400

    user models created and registrated on the admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = ('pk', 'user', 'phone_number', 'website', 'profile_picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('phone_number', 'website', 'profile_picture')
+
+    search_fields = ('user__first_name', 'user__last_name', 'user__email', 'phone_number')
+    list_filter = ('created', 'modified')
+
+    fieldsets = (
+        ('Profile', {
+            'fields' : (('user', 'profile_picture'),),
+        }),
+        ('Extra Info', {
+            'fields' : (
+                ('website', 'phone_number'),
+                ('bio')
+            )
+        }),
+        ('Dates', {
+            'fields' : (
+                ('birth_date'),
+                ('created'),
+                ('modified'),
+            )
+        })
+    )
+
+    readonly_fields = ('created', 'modified', 'user')
+
+
+class ProfileInline(admin.StackedInline):
+    
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    
+    inlines =(ProfileInline,)
+
+
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    birth_date = models.DateField(blank=False)
+    website = models.URLField(max_length=200)
+    bio = models.TextField(max_length=1000)
+    phone_number = models.CharField(max_length=15)
+
+    profile_picture = models.ImageField(upload_to='images/users')
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+    
+
+
